f839807a78dca3ac932c2837eabe773ab42ee087
Rabi Ahmed Home <ahmedrabi8@gmail.com>
2024-01-18 04:05:12 +0500

    Hero section

--- app/components/main/Hero.tsx
+++ app/components/main/Hero.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import HeroContent from '../sub/HeroContent'
 
 const Hero = () => {
   return (
     <div className='rotate flex flex-col h-full w-full'>
         <video
-        autoFocus
+        autoPlay
         muted
         loop
         className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
         >
             <source src='/blackhole.webm' type='video/webm' />
         </video>
+        <HeroContent />
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import Hero from './components/main/Hero'
+import Products from './components/main/Product'
 
 export default function Home() {
   return (
-    <main className='h-full w-full'>
+    <main className='h-full w-full margin-top-[400px]'>
       <div className='flex flex-col h-[850px] gap-20 '>
         <Hero />
+        {/* <div className='z-10'><Products /></div> */}
+        
       </div>
+
     </main>
   )
 }
